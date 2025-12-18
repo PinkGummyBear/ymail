47bb4711351be5def6692d4c456bd49cdaf51164
Vik <157316061+PinkGummyBear@users.noreply.github.com>
2025-12-18 20:20:47 +0100

    took out second path

--- server3.js
+++ server3.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const net = require('net');
 const tls = require('tls');
-const path = require('path');
 const { EventEmitter } = require('events');
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
@@ -2237,4 +2236,5 @@ process.on('unhandledRejection', (reason, promise) => {
 
 
 startServer();
+
 
